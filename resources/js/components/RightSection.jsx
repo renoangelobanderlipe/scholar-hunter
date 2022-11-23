9d70b14e673d914d9a9a2e031f596dbb37eac0e6
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2022-11-24 02:09:21 +0800

    Changed structure of the code preparing for the SPA implementation

---
+++ resources/js/components/RightSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const RightSection = () => {
+  
+  return (
+    <React.Fragment>
+      RIGHT SHIT
+    </React.Fragment>
+  );
+}
--- resources/js/layouts/PrimaryLayout.jsx
+++ resources/js/layouts/PrimaryLayout.jsx
@@ -1,23 +1,28 @@
-import { Grid } from "@mui/material";
 import React from "react";
+import { Grid } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+import { LeftSection } from "../components/LeftSection";
+import { RightSection } from "../components/RightSection";
 import { AppbarComponent } from "../components/wrapper/AppbarComponent";
 import { SidebarComponent } from "../components/wrapper/SidebarComponent";
 import { WrapperContainer } from "../components/wrapper/WrapperContainer";
+import { SignUpPage } from "../components/Auth/SignUpPage";
+import { SignInPage } from "../components/Auth/SignInPage";
 
-const drawerWidth = 200;
+// const drawerWidth = 200;
 export const PrimaryLayout = () => {
   return (
     <BrowserRouter>
       <Grid container spacing={2}>
-        <AppbarComponent drawerWidth={drawerWidth} />
-
+        {/* <AppbarComponent drawerWidth={drawerWidth} /> */}
         <Grid item xs={2}>
-          <SidebarComponent />
+          <RightSection />
         </Grid>
         <Grid item>
-          <WrapperContainer />
+          <LeftSection />
         </Grid>
+
+        {/* <SignInPage/> */}
       </Grid>
     </BrowserRouter>
   );
