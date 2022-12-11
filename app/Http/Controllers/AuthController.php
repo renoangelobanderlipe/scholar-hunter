1c83c393c7b141f60b65a66cb6325e42508c0440
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2022-12-11 23:27:19 +0800

    refs #11 setup laravel sanctum with sample AuthController data closes #11

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return 'Login Test';
+    }
+
+    public function register()
+    {
+        return 'Register Test';
+    }
+    
+    public function logout()
+    {
+        return 'logout Test';
+    }
+}
