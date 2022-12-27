eb033af567ce0a8acafe946287e492ab7b9bfc8e
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2022-12-27 14:34:43 +0800

    Sanctum SPA config

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,8 @@ window._ = _;
 import axios from 'axios';
 window.axios = axios;
 
+axios.defaults.withCredentials = true;
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
