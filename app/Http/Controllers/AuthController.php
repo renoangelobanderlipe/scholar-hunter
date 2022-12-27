5ea4b6e8c2e06bf8590601c22583ff68a3c07919
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2022-12-27 14:09:36 +0800

    refs #11 implemented laravel sanctum closes #11

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use App\Traits\HttpResponseTraits;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 class AuthController extends Controller
 {
-    public function login()
+    use HttpResponseTraits;
+
+    /**
+     * This uses a custom request to handle a customisable validation
+     * 
+     * 
+     */
+    public function login(LoginUserRequest $request)
     {
-        return 'Login Test';
+        $request->validated($request->all());
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Credentials do not match!', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of ' . $user->name)->plainTextToken
+        ]);
     }
 
-    public function register()
+    /**
+     * This uses a custom request to handle a customisable validation
+     * 
+     * 
+     */
+    public function register(StoreUserRequest $request)
     {
-        return 'Register Test';
+        $request->validated($request->only(['name', 'email', 'password']));
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of' . $user->name)->plainTextToken,
+        ]);
     }
-    
+
     public function logout()
     {
-        return 'logout Test';
+        Auth::user()->currentAccessToken()->delete();
+        return $this->success(['message' => 'Successfuly Logged out!']);
     }
 }
