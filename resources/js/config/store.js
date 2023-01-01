e4d8dabd5d2276f3fcdd8e5eda2e46de10b9b631
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-02 05:23:30 +0800

    added authentication state and theme

---
+++ resources/js/config/store.js
@@ -0,0 +1,63 @@
+import create from "zustand";
+import { persist } from 'zustand/middleware';
+
+const defaultAuthState = {
+  loggedIn: false,
+  // permissions : [] // This will be implemented later using the laravel sanctum abilities 
+}
+
+const useAuthStore = create(persist((set, get) => ({
+  ...defaultAuthState,
+  setLoggedIn: (data) => {
+    return set((state) => {
+      const tempState = {
+        loggedIn: true
+      };
+      return tempState;
+    })
+  },
+
+  setLoggedOut: (data) => {
+    return set((state) => {
+      return defaultAuthState;
+    })
+  }
+
+}), {
+  name: 'authStorage',
+  // getStorage: () =>
+  //   /**
+  //    * (optional) by default, 'local storage' is used
+  //    * but personally, i use sessionStorage so that it will only
+  //    * be stored in the current tab/window i am on and will not
+  //    * affect other tabs.
+  //    * 
+  //    * because of the 'persist' middle-ware, the above
+  //    * saveToBrowserStorage & takeFromBrowserStorage are now 
+  //    * obsolete
+  //    */
+  //   sessionStorage,
+}))
+
+const defaultThemeState = {
+  'theme': 'light'
+}
+
+const themeState = create(persist((set, get) => ({
+  ...defaultThemeState,
+  setToggle: (data) => {
+    return set((state) => {
+      return defaultAuthState.theme = 'dark';
+    });
+  },
+
+  setToggleOut: () => {
+    return set((state) => {
+      return defaultThemeState;
+    })
+  }
+
+})))
+
+
+export default useAuthStore;
