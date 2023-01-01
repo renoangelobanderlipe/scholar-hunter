e097319e44f4859fb51c0261ba1f0c70563ccd75
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-02 05:21:33 +0800

    added not found page for navigating to unknown url route

--- resources/js/components/NotFoundPage.jsx
+++ resources/js/components/NotFoundPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack'
+
 
 export const NotFoundPage = () => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar()
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    enqueueSnackbar('HAKDOG', { variant: 'success' })
+  }
+
   return (
     <>
       NOT FOUNDSHIT
+      <Button onClick={() => navigate(-1)}>Go Back</Button>
     </>
-  )l
+  )
 }
