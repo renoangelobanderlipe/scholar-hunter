14e49e883202841bf4bf5f37daffa631ed3efb68
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-02 05:23:44 +0800

    prepared for SPA shit

--- resources/js/pages/Auth/SignInPage.jsx
+++ resources/js/pages/Auth/SignInPage.jsx
@@ -1,13 +1,48 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
 import { useFormik } from "formik";
-import { GenericTextField } from "./../../components/GenericComponents/GenericTextField";
-import { GenericButton } from "./../../components/GenericComponents/GenericButton";
-import Typography from '@mui/material/Typography';
+import { GenericTextField } from "../../components/GenericComponents/TextField/GenericTextField";
+import { GenericButton } from "../../components/GenericComponents/Button/GenericButton";
 import { login } from './../../config/apisauce';
+import * as Yup from "yup";
+import { Grid, Box, Typography } from '@mui/material';
+import { GenericTypography } from './../../components/GenericComponents/Typography/GenericTypography';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+import useAuthStore from './../../config/store';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().min(6, 'Password must be at min of 6 characters').required('Required'),
+});
+
+const style = {
+  padding: '0.5rem 0',
+  textAlign: 'center',
+};
 
 
-export const SignInPage = () => {
+const headingVariant = {
+  variant: 'h4',
+  color: 'orange',
+  mb: '1rem'
+
+}
+const subHeadingVariant = {
+  size: 'small',
+  color: 'orange',
+  mb: '2rem'
+}
+
+const test = {
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+const SignInPage = () => {
+  const navigate = useNavigate();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const { setLoggedIn } = useAuthStore();
+
   const signInFormik = useFormik({
     initialValues: {
       email: "",
@@ -17,20 +52,22 @@ export const SignInPage = () => {
   });
 
   const handleOnSubmit = async (values) => {
-    // console.log('values', values);
-    const res = login({email: values.email, password : values.password});
+    await axios.get('sanctum/csrf-cookie');
 
-    // const res = await axios.post({ puser_data: values });
+    const res = await login({ email: values.email, password: values.password });
 
-    // if (res.data.code == 200) {
+    if (res.status == 200) {
+      enqueueSnackbar('Success', { variant: 'success' })
 
+      // SET LOGGED IN STATUS TO TRUE USING ZUSTAND! 
+      setLoggedIn(true);
 
+      navigate('/home', { replace: true });
 
-    //   signInFormik.values('');
-    // }
-    // console.log('result', result);
+    } else {
+      enqueueSnackbar(res.data.message, { variant: 'error' })
+    }
 
-    console.log('DONHE');
   };
 
   const handleOnChange = (field, newValue) => {
@@ -39,58 +76,97 @@ export const SignInPage = () => {
 
   return (
     <React.Fragment>
-      <form>
-        <GenericTextField
-          fieldName="email"
-          fieldLabel="Last Name"
-          handleOnChangeValue={(field, newValue) =>
-            handleOnChange(field, newValue)
-          }
-          variant={{
-            rows: 8,
-            variant: "outlined",
-            size: "small",
-          }}
-          fieldOptions={{
-            placeholder: "Email",
-            type: "email",
-          }}
-        // formikErrors={{
-        //   error: signUpFormik.errors?.purpose ? true : false,
-        //   helperText: signUpFormik.errors?.purpose,
-        // }}
-        />
-        <GenericTextField
-          fieldName="password"
-          fieldLabel="Password"
-          handleOnChangeValue={(field, newValue) =>
-            handleOnChange(field, newValue)
-          }
-          variant={{
-            rows: 8,
-            variant: "outlined",
-            size: "small",
-          }}
-          fieldOptions={{
-            placeholder: "Password",
-            type: "password",
-          }}
-        // formikErrors={{
-        //   error: signUpFormik.errors?.purpose ? true : false,
-        //   helperText: signUpFormik.errors?.purpose,
-        // }}
-        />
+      <Grid item margin={'20%'} padding={'2rem'} >
+        <Box>
+          <Grid item sx={style}>
+            <GenericTypography
+              title={'Hello Again!'}
+              variant={headingVariant}
+            />
+          </Grid>
+          <Grid item sx={style}>
+            <GenericTypography
+              title={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'}
+              variant={subHeadingVariant}
+            />
+          </Grid>
+        </Box>
 
-        <GenericButton
-          title={"Sign In"}
-          variant={{
-            variant: "text",
-            color: "primary",
-          }}
-          onClick={() => handleOnSubmit(signInFormik.values)}
-        />
-      </form>
+        <form>
+          <Grid item sx={style}>
+            <GenericTextField
+              fieldName="email"
+              fieldLabel="Last Name"
+              handleOnChangeValue={(field, newValue) =>
+                handleOnChange(field, newValue)
+              }
+              variant={{
+                rows: 8,
+                variant: "outlined",
+                fullWidth: true,
+                size: "small",
+              }}
+              fieldOptions={{
+                placeholder: "Email",
+                type: "email",
+              }}
+              formikErrors={{
+                error: signInFormik.errors?.purpose ? true : false,
+                helperText: signInFormik.errors?.purpose,
+              }}
+            />
+          </Grid >
 
+          <Grid item sx={style}>
+            <GenericTextField
+              fieldName="password"
+              fieldLabel="Password"
+              handleOnChangeValue={(field, newValue) =>
+                handleOnChange(field, newValue)
+              }
+              variant={{
+                rows: 8,
+                fullWidth: true,
+                variant: "outlined",
+                size: "small",
+              }}
+              fieldOptions={{
+                placeholder: "Password",
+                type: "password",
+              }}
+              formikErrors={{
+                error: signInFormik.errors?.purpose ? true : false,
+                helperText: signInFormik.errors?.purpose,
+              }}
+            />
+          </Grid>
+
+          <Grid item sx={style} mt={'2rem'} mb={'2rem'}>
+            <GenericButton
+              title={"Sign In"}
+              variant={{
+                fullWidth: true,
+                variant: "contained",
+                color: "primary",
+              }}
+              onClick={() => handleOnSubmit(signInFormik.values)}
+            />
+          </Grid>
+
+          <Grid container sx={test}>
+            <GenericTypography
+              variant={{
+              }}
+              title={'Don\'t have account yet?'}
+            />
+            <Grid>
+              <Link to='/register'>Sign Up</Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
     </React.Fragment>
   );
 };
+
+export default SignInPage;
--- resources/js/pages/Auth/SignUpPage.jsx
+++ resources/js/pages/Auth/SignUpPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import Button from "@mui/material/Button";
-import axios from "axios";
-import { GenericTextField } from './../../components/GenericComponents/GenericTextField';
-import { GenericButton } from './../../components/GenericComponents/GenericButton';
+import { GenericTextField } from '../../components/GenericComponents/TextField/GenericTextField';
+import { GenericButton } from '../../components/GenericComponents/Button/GenericButton';
+import { Grid, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { GenericTypography } from './../../components/GenericComponents/Typography/GenericTypography';
+import { register } from './../../config/apisauce';
 
 const validationSchema = Yup.object().shape({
   // firstName: Yup.string()
@@ -18,16 +20,47 @@ const validationSchema = Yup.object().shape({
   // email: Yup.string().email('Invalid email').required('Required'),
 });
 
-export const SignUpPage = () => {
-  const signUpFormik = useFormik({
+
+const style = {
+  padding: '0.5rem 0',
+  textAlign: 'center',
+};
+
+
+const headingVariant = {
+  variant: 'h4',
+  color: 'orange',
+  mb: '1rem'
+
+}
+const subHeadingVariant = {
+  size: 'small',
+  color: 'orange',
+  mb: '2rem'
+
+}
+
+const test = {
+  // backgroundColor : 'orange',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+const boxDesign = {
+  display: 'flex'
+}
+
+const SignUpPage = () => {
+  const registerFormik = useFormik({
     initialValues: {
-      firstname: "",
-      lastname: "",
       email: "",
-      password: "",
       username: "",
-      email: "",
+      firstname: "",
+      middlename: "",
+      lastname: "",
       address: "",
+      password: "",
+      confirm_password: "",
     },
 
     validationSchema: validationSchema,
@@ -41,8 +74,10 @@ export const SignUpPage = () => {
   });
 
   const handleOnSubmit = async (values) => {
-    console.log("Sign In DATA RESULT", values);
-    // const result = await axios.post({'user_data' : values});
+    console.log('test', values
+    );
+
+    // const res = await register({});
 
     /**
      *
@@ -60,6 +95,7 @@ export const SignUpPage = () => {
       // THROW EXCEPTION SHIT
     }
     // REDIRECT WITH SUCCESS MESSAGE
+
   };
 
   const handleOnChange = (field, newValue) => {
@@ -68,137 +104,240 @@ export const SignUpPage = () => {
 
   return (
     <React.Fragment>
-      <form>
-        <GenericTextField
-          fieldName="firstname"
-          fieldLabel="First Name"
-          handleOnChangeValue={(field, newValue) =>
-            handleOnChange(field, newValue)
-          }
-          variant={{
-            rows: 8,
-            variant: "outlined",
-            size: "small",
-          }}
-          fieldOptions={{
-            placeholder: "First Name",
-            type: "text",
-          }}
-          // formikErrors={{
-          //   error: signUpFormik.errors?.purpose ? true : false,
-          //   helperText: signUpFormik.errors?.purpose,
-          // }}
-        />
-        <GenericTextField
-          fieldName="lastname"
-          fieldLabel="Last Name"
-          handleOnChangeValue={(field, newValue) =>
-            handleOnChange(field, newValue)
-          }
-          variant={{
-            rows: 8,
-            variant: "outlined",
-            size: "small",
-          }}
-          fieldOptions={{
-            placeholder: "Last Name",
-            type: "text",
-          }}
-          // formikErrors={{
-          //   error: signUpFormik.errors?.purpose ? true : false,
-          //   helperText: signUpFormik.errors?.purpose,
-          // }}
-        />
-        <GenericTextField
-          fieldName="address"
-          fieldLabel="Address"
-          handleOnChangeValue={(field, newValue) =>
-            handleOnChange(field, newValue)
-          }
-          variant={{
-            rows: 8,
-            variant: "outlined",
-            size: "small",
-          }}
-          fieldOptions={{
-            placeholder: "Address",
-            type: "Field Type",
-          }}
-          // formikErrors={{
-          //   error: signUpFormik.errors?.purpose ? true : false,
-          //   helperText: signUpFormik.errors?.purpose,
-          // }}
-        />
-        <GenericTextField
-          fieldName="email"
-          fieldLabel="Email"
-          handleOnChangeValue={(field, newValue) =>
-            handleOnChange(field, newValue)
-          }
-          variant={{
-            rows: 8,
-            variant: "outlined",
-            size: "small",
-          }}
-          fieldOptions={{
-            placeholder: "Email",
-            type: "email",
-          }}
-          // formikErrors={{
-          //   error: signUpFormik.errors?.purpose ? true : false,
-          //   helperText: signUpFormik.errors?.purpose,
-          // }}
-        />
-        <GenericTextField
-          fieldName="username"
-          fieldLabel="User Name"
-          handleOnChangeValue={(field, newValue) =>
-            handleOnChange(field, newValue)
-          }
-          variant={{
-            rows: 8,
-            variant: "outlined",
-            size: "small",
-          }}
-          fieldOptions={{
-            placeholder: "User Name",
-            type: "text",
-          }}
-          // formikErrors={{
-          //   error: signUpFormik.errors?.purpose ? true : false,
-          //   helperText: signUpFormik.errors?.purpose,
-          // }}
-        />
-        <GenericTextField
-          fieldName="password"
-          fieldLabel="Password"
-          handleOnChangeValue={(field, newValue) =>
-            handleOnChange(field, newValue)
-          }
-          variant={{
-            rows: 8,
-            variant: "outlined",
-            size: "small",
-          }}
-          fieldOptions={{
-            placeholder: "Password",
-            type: "password",
-          }}
-          // formikErrors={{
-          //   error: signUpFormik.errors?.purpose ? true : false,
-          //   helperText: signUpFormik.errors?.purpose,
-          // }}
-        />
+      <Grid item margin={'20%'}  >
+        <Box>
+          <Grid item sx={style}>
+            <GenericTypography
+              title={'Hello Again!'}
+              variant={headingVariant}
+            />
+          </Grid>
+          <Grid item sx={style}>
+            <GenericTypography
+              variant={subHeadingVariant}
+            />
+          </Grid>
+        </Box>
 
-        <GenericButton
-          title={"Sign Up"}
-          variant={{
-            variant: "text",
-            color: "primary",
-          }}
-          onClick={() => signUpFormik.handleSubmit(signUpFormik.values)}
-        />
-      </form>
+        <form>
+          <Box sx={boxDesign}>
+            <Grid item sx={style}>
+              <GenericTextField
+                fieldName="email"
+                fieldLabel="Email"
+                handleOnChangeValue={(field, newValue) =>
+                  handleOnChange(field, newValue)
+                }
+                variant={{
+                  rows: 8,
+                  variant: "outlined",
+                  fullWidth: true,
+                  size: "small",
+                }}
+                fieldOptions={{
+                  placeholder: "Email",
+                }}
+                formikErrors={{
+                  error: registerFormik.errors?.purpose ? true : false,
+                  helperText: registerFormik.errors?.purpose,
+                }}
+              />
+            </Grid >
+
+            <Grid item sx={style}>
+              <GenericTextField
+                fieldName="username"
+                fieldLabel="Username"
+                handleOnChangeValue={(field, newValue) =>
+                  handleOnChange(field, newValue)
+                }
+                variant={{
+                  rows: 8,
+                  variant: "outlined",
+                  fullWidth: true,
+                  size: "small",
+                }}
+                fieldOptions={{
+                  placeholder: "Username",
+                }}
+                formikErrors={{
+                  error: registerFormik.errors?.purpose ? true : false,
+                  helperText: registerFormik.errors?.purpose,
+                }}
+              />
+            </Grid >
+          </Box>
+
+          <Box sx={boxDesign}>
+            <Grid item sx={style}>
+              <GenericTextField
+                fieldName="firstname"
+                fieldLabel="First Name"
+                handleOnChangeValue={(field, newValue) =>
+                  handleOnChange(field, newValue)
+                }
+                variant={{
+                  rows: 8,
+                  variant: "outlined",
+                  fullWidth: true,
+                  size: "small",
+                }}
+                fieldOptions={{
+                  placeholder: "First Name",
+                }}
+                formikErrors={{
+                  error: registerFormik.errors?.purpose ? true : false,
+                  helperText: registerFormik.errors?.purpose,
+                }}
+              />
+            </Grid >
+
+            <Grid item sx={style}>
+              <GenericTextField
+                fieldName="middlename"
+                fieldLabel="Middle Name"
+                handleOnChangeValue={(field, newValue) =>
+                  handleOnChange(field, newValue)
+                }
+                variant={{
+                  rows: 8,
+                  variant: "outlined",
+                  fullWidth: true,
+                  size: "small",
+                }}
+                fieldOptions={{
+                  placeholder: "Middle Name",
+                }}
+                formikErrors={{
+                  error: registerFormik.errors?.purpose ? true : false,
+                  helperText: registerFormik.errors?.purpose,
+                }}
+              />
+            </Grid >
+          </Box>
+
+          <Grid item sx={style}>
+            <GenericTextField
+              fieldName="lastname"
+              fieldLabel="Last Name"
+              handleOnChangeValue={(field, newValue) =>
+                handleOnChange(field, newValue)
+              }
+              variant={{
+                rows: 8,
+                variant: "outlined",
+                fullWidth: true,
+                size: "small",
+              }}
+              fieldOptions={{
+                placeholder: "Last Name",
+              }}
+              formikErrors={{
+                error: registerFormik.errors?.purpose ? true : false,
+                helperText: registerFormik.errors?.purpose,
+              }}
+            />
+          </Grid >
+
+          <Grid item sx={style}>
+            <GenericTextField
+              fieldName="address"
+              fieldLabel="Address"
+              handleOnChangeValue={(field, newValue) =>
+                handleOnChange(field, newValue)
+              }
+              variant={{
+                rows: 8,
+                variant: "outlined",
+                fullWidth: true,
+                size: "small",
+              }}
+              fieldOptions={{
+                placeholder: "Address",
+              }}
+              formikErrors={{
+                error: registerFormik.errors?.purpose ? true : false,
+                helperText: registerFormik.errors?.purpose,
+              }}
+            />
+          </Grid >
+
+
+          <Box display='flex'>
+            <Grid item sx={style}>
+              <GenericTextField
+                fieldName="password"
+                fieldLabel="Password"
+                handleOnChangeValue={(field, newValue) =>
+                  handleOnChange(field, newValue)
+                }
+                variant={{
+                  rows: 8,
+                  fullWidth: true,
+                  variant: "outlined",
+                  size: "small",
+                }}
+                fieldOptions={{
+                  placeholder: "Password",
+                }}
+              // formikErrors={{
+              //   error: registerFormik.errors?.purpose ? true : false,
+              //   helperText: registerFormik.errors?.purpose,
+              // }}
+              />
+            </Grid>
+
+            <Grid item sx={style}>
+              <GenericTextField
+                fieldName="confirm_password"
+                fieldLabel="Confirm Password"
+                handleOnChangeValue={(field, newValue) =>
+                  handleOnChange(field, newValue)
+                }
+                variant={{
+                  rows: 8,
+                  fullWidth: true,
+                  variant: "outlined",
+                  size: "small",
+                }}
+                fieldOptions={{
+                  placeholder: "Confirm Password",
+                }}
+              // formikErrors={{
+              //   error: registerFormik.errors?.purpose ? true : false,
+              //   helperText: registerFormik.errors?.purpose,
+              // }}
+              />
+            </Grid>
+          </Box>
+
+
+          <Grid item sx={style} mt={'2rem'} mb={'2rem'}>
+            <GenericButton
+              title={"Sign In"}
+              variant={{
+                fullWidth: true,
+                variant: "contained",
+                color: "primary",
+              }}
+              onClick={() => handleOnSubmit(registerFormik.values)}
+            />
+          </Grid>
+
+          <Grid container sx={test}>
+            <Link to='/login'>
+              <GenericTypography
+                variant={{
+                }}
+                title={'Already have an account?'}
+              />
+            </Link>
+          </Grid>
+        </form>
+      </Grid>
     </React.Fragment>
   );
 };
+
+
+export default SignUpPage;
