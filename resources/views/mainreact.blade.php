5dd530e1377b217a4c9910095d5d2d82d931d973
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-04 23:22:25 +0800

    for issuing bearer token to the request headers

--- resources/views/mainreact.blade.php
+++ resources/views/mainreact.blade.php
@@ -1,27 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{env("APP_ENV") === "local" ? "[LOCAL] " : ""}}{{env("APP_NAME")}}</title>
-  <!-- <script type="text/javascript">
-    const app = {
-      !!
+  <script type="text/javascript">
+    const app = {!!
       json_encode([
         "name" => env("APP_NAME"),
         "env" => env("APP_ENV"),
         "url" => url('/'),
         "csrf" => csrf_token()
-      ], JSON_PRETTY_PRINT);!!
-    };
-  </script> -->
+      ], JSON_PRETTY_PRINT);
+    !!};
+  </script>
 </head>
-
 <body>
   <div id="root">you need to enable javascript to load this page.</div>
   @vite(['resources/js/app.jsx'])
 </body>
-
 </html>
