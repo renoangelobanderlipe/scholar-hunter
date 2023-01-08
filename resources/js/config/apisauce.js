70216d84213dfd3165fc2b37bba5b125f634222c
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-08 08:19:29 +0800

    added required Routes

--- resources/js/config/apisauce.js
+++ resources/js/config/apisauce.js
@@ -13,3 +13,13 @@ export const register = (params) => apiSauce.post('/api/register', params);
 export const login = (params) => apiSauce.post('/api/login', params);
 export const logout = () => apiSauce.post('/api/logout');
 
+// User Management
+export const createUser = (params) => apiSauce.post('api/user-mamangement/create', params);
+
+// Scholarship Management 
+export const createScholarship = (params) => apiSauce.post('api/scholarship-management/create', params);
+
+// Profile Information
+export const showProfile = () => apiSauce.post('api/profile/show')
+export const profileUpdate = (params) => apiSauce.post('api/profile/update');
+export const updatePassword = (params) => apiSauce.post('api/profile/password-update');
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Profile\ProfileController;
+use App\Http\Controllers\ScholarshipManagement\ScholarshipManagementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +17,24 @@ Route::post('/register', [AuthController::class, 'register']);
 // Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// Scholarship Type 
+Route::post('scholarship-type/show', App\Http\Controllers\ScholarshipType\ScholarshipTypeController::class);
+
+// User Management
+Route::prefix('user-management')->group(function () {
+    Route::post('/create', [UserManagementController::class, 'create']);
+});
+
+// Scholarship Management
+Route::prefix('scholarship-management')->group(function () {
+    Route::post('create', [ScholarshipManagementController::class, 'create']);
+});
+
+// Profile Information
+Route::prefix('profile')->group(function () {
+    Route::post('/show', [ProfileController::class, 'show']);
+    Route::post('/update', [ProfileController::class, 'update']);
+    Route::post('/password-update', [ProfileController::class, 'password']);
 });
