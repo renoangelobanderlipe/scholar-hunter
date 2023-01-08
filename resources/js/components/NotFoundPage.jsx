dff27e76ae02dd1c6d45126bbc0a1b1be88fe48c
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-08 08:15:57 +0800

    redesigned page

--- resources/js/components/NotFoundPage.jsx
+++ resources/js/components/NotFoundPage.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
-import { Button, Card, CardContent, CardMedia, Stack, Typography } from '@mui/material';
-import { Navigate, useNavigate, Link } from 'react-router-dom';
-import { useSnackbar } from 'notistack'
+import { Button, Card, CardContent, CardMedia, Stack } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import { Grid } from '@mui/material';
 
 
 export const NotFoundPage = () => {
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar()
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    enqueueSnackbar('HAKDOG', { variant: 'success' })
-  }
-
   return (
-    <>
+    <React.Fragment>
       <Stack alignItems='center'>
         <Card sx={{ backgroundColor: 'transparent' }}>
           <CardMedia component="iframe" sx={{ minHeight: '450px', minWidth: '350px', height: '50vh', width: '40vw', border: 'none' }} src="https://embed.lottiefiles.com/animation/89922"></CardMedia>
-          <Grid container justifyContent ='center'>
+          <Grid container justifyContent='center'>
             <CardContent >
               <Button onClick={() => navigate(-1)}>Go Back</Button>
             </ CardContent>
           </Grid>
         </Card>
       </Stack>
-    </>
+    </React.Fragment>
   )
 }
