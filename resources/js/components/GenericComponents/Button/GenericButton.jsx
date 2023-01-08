eba89cd91293b709e870d3e8833b66c767d37931
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-08 08:16:44 +0800

    added others props

--- resources/js/components/GenericComponents/Button/GenericButton.jsx
+++ resources/js/components/GenericComponents/Button/GenericButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Button from "@mui/material/Button";
 
-export const GenericButton = ({ title, variant, onClick }) => {
+export const GenericButton = ({ title, variant, onClick, others }) => {
   return (
     <React.Fragment>
-      <Button {...variant} onClick={onClick}>
+      <Button {...variant} {...others} onClick={onClick}>
         {title}
       </Button>
     </React.Fragment>
