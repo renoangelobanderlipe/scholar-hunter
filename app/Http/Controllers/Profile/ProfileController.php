67950e9a49754c9b386dfd7cb5b55bde59313dd0
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-08 22:12:05 +0800

    test

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -15,7 +16,17 @@ class ProfileController extends Controller
 
     public function update(Request $request)
     {
-        // 
+        $request = $request->only(['current_password', 'password', 'confirm_password']);
+
+        if ($request['password'] !=  $request['confirm_password']) {
+            return response()->json('Password Mismatch', 201);
+        };
+
+        User::select('id', 'password')
+            ->where('password', '=', $request['current_password'])
+            ->update(['password' => $request['password']]);
+
+        return response()->json('Success', 200);
     }
 
     public function password(Request $request)
