15168d42dc4a8ae3cce1b96dd243d17a3a8bcc91
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-21 09:47:31 +0800

    refactor this shit

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -12,20 +12,8 @@ class ProfileController extends Controller
 {
     use HttpResponseTraits;
 
-    /**
-     * The "booted" method of the model.
-     *
-     * @return void
-     */
-    protected static function booted()
-    {
-        static::addGlobalScope(new UserScope);
-    }
-
     public function index()
     {
-        dd('test global scopes', User::select('*')->apply());
-        // Send back user info to  
         try {
         } catch (\Throwable $throwable) {
             $this->error($throwable->getMessage());
@@ -43,20 +31,8 @@ class ProfileController extends Controller
     public function updatePassword(Request $request)
     {
         try {
-            $request = $request->only(['current_password', 'password', 'confirm_password']);
-
-
-            if ($request['confirm_password'] !=  $request['confirm_password']) {
-                return response()->json('Password Mismatch', 201);
-            };
-
-            User::select('id', 'password')
-                ->where('password', '=', $request['current_password'])
-                ->update(['password' => $request['password']]);
-
-            return $this->success('Successfully Updated Information');
         } catch (\Throwable $throwable) {
-            return $this->error($throwable->getMessage());
+            $this->error($throwable->getMessage());
         }
     }
 }
