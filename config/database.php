2be74de3fed764ed8fb35ffe3b81737f96de9364
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-26 22:54:41 +0800

    refs #18 added spatie package for permission and role closes #18

--- config/database.php
+++ config/database.php
@@ -57,7 +57,9 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB',
+            'charset' => 'utf8',
+            'collation' => 'utf8_general_ci',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
@@ -125,7 +127,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
