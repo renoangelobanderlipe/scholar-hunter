c2fd16a85b378ea7f2d3335960e59be14e322f3c
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-28 10:15:03 +0800

    refs  #24 api implementation for auth closes  #24

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'register');
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Authentication
+    Route::controller(AuthController::class)->prefix('auth')->group(function () {
+        Route::post('login', 'login');
+        Route::post('logout', 'logout');
+    });
 });
