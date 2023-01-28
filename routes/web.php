26931db19960f5c88bdd4e28e6ddfa1c6616cf34
Reno Angelo Banderlipe <peanut.renoangelo.banderlipe@gmail.com>
2023-01-28 10:16:30 +0800

    refs #26 frontend routing closes #26

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/{any?}', function () {
+    return view('mainreact');
+})->where('any', '.*')->name('/');
