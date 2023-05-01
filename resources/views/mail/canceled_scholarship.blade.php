3cd86894d826691d61247f25852871b34b3e71ff
Noel Xavier Ulpindo <noel.ulpindo@gmail.com>
2023-05-02 01:43:56 +0800

    updated message

--- resources/views/mail/canceled_scholarship.blade.php
+++ resources/views/mail/canceled_scholarship.blade.php
@@ -1,1 +1,17 @@
-Unfortunately Your Scholarship Application was Rejected.
+Dear Students, 
+
+  We have reviewed your requirements and there is/are a missing file/s.
+
+  Please make sure that the following needed documents are compiled together : 
+
+  FHE Form
+  Certificate of Grades
+  Ceritificate of Enrollment
+
+  Once checked, please resubmit them by reuploading the file using the system.
+
+  Thank you very much.
+
+  Sincerly,
+
+  Admin
